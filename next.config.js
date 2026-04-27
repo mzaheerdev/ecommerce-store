0074ce4f7fc01a0d58c993d06396e71ec265662c
mzaheerkhan <mzaheerkhan000@gmail.com>
2026-04-27 13:29:23 +0500

    initail commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "i.ibb.co",
+      "lh3.googleusercontent.com",
+      "res.cloudinary.com",
+      "images.unsplash.com",
+    ],
+  },
+};
+
+module.exports = nextConfig;
