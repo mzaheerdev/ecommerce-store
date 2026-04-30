f85695e0a5e31e85b5b2ea5b1439729a45af6043
mzaheerkhan <mzaheerkhan000@gmail.com>
2026-05-01 00:19:38 +0500

    vercel errors

--- src/pages/home-4.jsx
+++ src/pages/home-4.jsx
@@ -12,7 +12,7 @@ import BestSellerPrd from '@/components/products/jewelry/best-seller-prd';
 import JewelryBrands from '@/components/brand/jewelry-brands';
 import InstagramAreaFour from '@/components/instagram/instagram-area-4';
 import FeatureAreaThree from '@/components/features/feature-area-3';
-import FooterTwo from '@/layout/footers/footer-2';
+import FooterTwo from '@/layout/footers/footer';
 
 const HomeFour = () => {
   return (
